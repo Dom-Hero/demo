569379c875d1e6c8a77d7258bb9b61b808867519
YZH <18569812260@163.com>
2026-06-11 13:35:52 +0800

    首次发布

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/demo/',   // 注意：这里是你的仓库名 demo
   plugins: [
     vue(),
     vueDevTools(),
